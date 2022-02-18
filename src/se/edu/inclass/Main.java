a5f8bc72ff86609962504a0a7687aa65db50cb6a
Bryan-BC <bryanchristopherbc88@gmail.com>
2022-02-18 17:25:02 +0800

    Add streams method

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -15,10 +15,16 @@ public class Main {
         DataManager dm = new DataManager("./data/data.txt");
         ArrayList<Task> tasksData = dm.loadData();
 
+        System.out.println("Printing tasks");
+        printData(tasksData);
+        printDataStreams(tasksData);
+
         System.out.println("Printing deadlines");
         printDeadlines(tasksData);
+        printDeadlinesStreams(tasksData);
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+        System.out.println("Total number of deadlines: " + countDeadlinesStreams(tasksData));
 
     }
 
@@ -32,10 +38,24 @@ public class Main {
         return count;
     }
 
+    private static int countDeadlinesStreams(ArrayList<Task> tasks) {
+        int count = 0;
+        count = (int) tasks.stream()
+                .filter((t) -> t instanceof Deadline)
+                .count();
+        return count;
+    }
+
     public static void printData(ArrayList<Task> tasksData) {
         for (Task t : tasksData) {
-            System.out.println(t);
+            System.out.println(t); // method call
         }
+    }
+
+    public static void printDataStreams(ArrayList<Task> tasks) {
+        System.out.println("\nPrint data using streams");
+        tasks.stream()              //convert tasks to stream (not collections anymore)
+                .forEach(System.out::println);         //terminal operation
     }
 
     public static void printDeadlines(ArrayList<Task> tasksData) {
